4aeaf843f4afb3cc276b9efb6f66c7a9e9bc6e71
Filip Pawelec <fpawelec0@gmail.com>
2024-04-25 10:18:15 +0200

    Add project files.

---
+++ CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,14 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+using namespace matplotlibcpp;
+int main()
+{
+	
+	plot({ 1, 2, 3, 4 }, "k");
+	show();
+	return 0;
+}
